0244eba0b38a1c44532412cea2915401b4021f67
atammara <mtasawor68@gamil.com>
2024-12-09 00:28:10 +0500

    files

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -72,7 +72,7 @@ const Blog: React.FC = () => {
       </div>
 
       {/* Blog Grid Section */}
-      <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+      <div className="container mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 mt-8">
         {blogs.map((blog) => (
           <div
             key={blog.id}
